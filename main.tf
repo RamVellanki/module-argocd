b5ae5708922cf96bee91a2e8800edcf291b14a85
Ramachandra Vellanki <vellanki.chandra@gmail.com>
2021-08-16 17:38:17 +0530

    added argocd terraform code

---
+++ main.tf
@@ -0,0 +1,28 @@
+provider "kubernetes" {
+  host                   = var.kubernetes_cluster_endpoint
+  client_certificate     = base64decode(var.client_certificate)
+  client_key             = base64decode(var.client_key)
+  cluster_ca_certificate = base64decode(var.cluster_ca_certificate)
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = var.kubernetes_cluster_endpoint
+    client_certificate     = base64decode(var.client_certificate)
+    client_key             = base64decode(var.client_key)
+    cluster_ca_certificate = base64decode(var.cluster_ca_certificate)
+  }
+}
+
+resource "kubernetes_namespace" "example" {
+  metadata {
+    name = "argo"
+  }
+}
+
+resource "helm_release" "argocd" {
+  name       = "msur"
+  chart      = "argo-cd"
+  repository = "https://argoproj.github.io/argo-helm"
+  namespace  = "argo"
+}
